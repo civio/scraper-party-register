c9f01921d73f21fa3f4effa25d8245ec6ee09f07
David Cabo <david.cabo@gmail.com>
2015-05-30 11:22:05 +0200

    Add template for morph.io scraper

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# It's easy to add more libraries or choose different versions. Any libraries
+# specified here will be installed and made available to your morph.io scraper.
+# Find out more: https://morph.io/documentation/ruby
+
+source "https://rubygems.org"
+
+ruby "2.0.0"
+
+gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
+gem "mechanize"
---
+++ scraper.rb
@@ -0,0 +1,25 @@
+# This is a template for a Ruby scraper on morph.io (https://morph.io)
+# including some code snippets below that you should find helpful
+
+# require 'scraperwiki'
+# require 'mechanize'
+#
+# agent = Mechanize.new
+#
+# # Read in a page
+# page = agent.get("http://foo.com")
+#
+# # Find somehing on the page using css selectors
+# p page.at('div.content')
+#
+# # Write out to the sqlite database using scraperwiki library
+# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+#
+# # An arbitrary query against the database
+# ScraperWiki.select("* from data where 'name'='peter'")
+
+# You don't have to do things with the Mechanize or ScraperWiki libraries.
+# You can use whatever gems you want: https://morph.io/documentation/ruby
+# All that matters is that your final data is written to an SQLite database
+# called "data.sqlite" in the current working directory which has at least a table
+# called "data".
