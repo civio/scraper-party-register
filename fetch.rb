3eddb8ed10983265ba53a1cab36d851534fe283a
David Cabo <david.cabo@gmail.com>
2015-05-30 17:05:57 +0200

    Recover from timeouts

--- fetch.rb
+++ fetch.rb
@@ -6,6 +6,7 @@ require 'mechanize'
 require 'csv'
 
 # Allow overriding of starting id from command line
+# Alternatively, we could skip existing files, unless a --force option is given in the command line
 start_id = ARGV[0] ? ARGV[0].to_i : 1
 
 agent = Mechanize.new
@@ -14,7 +15,13 @@ start_id.upto(6000) do |id|
   puts "Fetching party #{id}..."
 
   # Read index page, just so the id is set in the session (who makes these websites?!)
-  agent.get("http://servicio.mir.es/nfrontal/webpartido_politico/partido_politicoDatos.html?nmformacion=#{id}")
+  begin
+    agent.get("http://servicio.mir.es/nfrontal/webpartido_politico/partido_politicoDatos.html?nmformacion=#{id}")
+  rescue Errno::ETIMEDOUT, Timeout::Error
+    puts "Timeout #{id}. Retrying..."
+    sleep 5
+    retry
+  end
 
   # Now we can get the page with the actual info, which now will have the party details
   begin
@@ -22,6 +29,11 @@ start_id.upto(6000) do |id|
     File.open("staging/#{id}.html", 'w') {|f| f.write(page.content) }
   rescue Mechanize::ResponseCodeError => e
     puts "Ignoring #{id}: #{e}"
+  rescue Errno::ETIMEDOUT, Timeout::Error
+    puts "Timeout #{id}. Retrying..."
+    sleep 5
+    retry
   end
 
+  sleep 2
 end
