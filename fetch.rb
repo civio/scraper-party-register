a994ec8102944d6360aacb018bf0aab415b81451
David Cabo <david.cabo@gmail.com>
2015-06-01 19:14:16 +0200

    Download associated pictures

--- fetch.rb
+++ fetch.rb
@@ -25,8 +25,22 @@ start_id.upto(6000) do |id|
 
   # Now we can get the page with the actual info, which now will have the party details
   begin
-    page = agent.get("http://servicio.mir.es/nfrontal/webpartido_politico/recurso/partido_politicoDetalle.html")
-    File.open("staging/#{id}.html", 'w') {|f| f.write(page.content) }
+    @page = agent.get("http://servicio.mir.es/nfrontal/webpartido_politico/recurso/partido_politicoDetalle.html")
+    File.open("staging/#{id}.html", 'w') {|f| f.write(@page.content) }
+  rescue Mechanize::ResponseCodeError => e
+    puts "Ignoring #{id}: #{e}"
+  rescue Errno::ETIMEDOUT, Timeout::Error
+    puts "Timeout #{id}. Retrying..."
+    sleep 5
+    retry
+  end
+
+  # Download the associated picture, if available
+  # XXX: We assume that they are all in JPEG format (".jpg" extension), seems to work.
+  next if @page.search('#simbolo').empty?
+  begin
+    picture = agent.get("http://servicio.mir.es/nfrontal/webpartido_politico/recurso/webpartido_politico/recurso/descargarImagen.html")
+    picture.save("pictures/#{id}.jpg")
   rescue Mechanize::ResponseCodeError => e
     puts "Ignoring #{id}: #{e}"
   rescue Errno::ETIMEDOUT, Timeout::Error
